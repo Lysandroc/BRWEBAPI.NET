00cd66dd38018331064bf89f56b42212e8494ef1
Lysandro Carioca <lysandroc@gmail.com>
2015-03-01 14:21:49 -0300

    Video aula 6, criação da camada demada de restfull com web com webapi utilizando owin e já injetando as dependencias do projeto

---
+++ SpaUserControl.Api/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System.Configuration;
+using System.Web.Hosting;
+using System.Web.Http;
+using Microsoft.Practices.ObjectBuilder2;
+using Microsoft.Practices.Unity;
+using Owin;
+using Owin.Loader;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Host.SystemWeb;
+using Owin.Builder;
+using SpaUserControl.Api.Helpers;
+using SpaUserControl.Startup;
+
+namespace SpaUserControl.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            var container = new UnityContainer();
+            DependencyResolver.Resolver(container);
+            config.DependencyResolver = new UnityResolver(container);
+
+            ConfigureWebApi(config);
+
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        public static void ConfigureWebApi(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{api}",
+                defaults: new {id = RouteParameter.Optional }
+                );
+        }
+    }
+}
