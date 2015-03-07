5e20299b1fb32f495f84a7172775e992df87a06d
Lysandro Carioca <lysandroc@gmail.com>
2015-03-07 14:13:12 -0300

    Aula 7 - Implementando owin e autenticação

---
+++ SpaUserControl.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SpaUserControl.WebApi.WebApiApplication" Language="C#" %>
---
+++ SpaUserControl.WebApi/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Microsoft.Practices.Unity;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using SpaUserControl.Domain.Contracts.Services;
+using SpaUserControl.Startup;
+
+namespace SpaUserControl.WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+            HttpConfiguration config = new HttpConfiguration();
+
+            // Configure Dependency Injection
+            var container = new UnityContainer();
+            DependencyResolver.Resolve(container);
+            config.DependencyResolver = new UnityResolver(container);
+
+            ConfigureWebApi(config);
+            ConfigureOAuth(app, container.Resolve<IUserService>());
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+
+        }
+      
+        public static void ConfigureWebApi(HttpConfiguration config)
+        {
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+        public void ConfigureOAuth(IAppBuilder app, IUserService service)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/api/security/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(2),
+                Provider = new SimpleAuthorizationServerProvider(service)
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
